c98d9203817b452a212d46e67f0d06910445545e
karub360 <karub360@gmail.com>
2025-11-21 00:33:13 +0300

    Animasyonlu harf efekti, gokkusagi degradesi ve arkaplan karartma katmani eklendi

--- masal-frontend/src/pages/Hosgeldin.js
+++ masal-frontend/src/pages/Hosgeldin.js
@@ -6,28 +6,37 @@ function Hosgeldin({ onNext }) {
     <div className="hosgeldin" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className="overlay">
         <div className="title-wrapper">
-          <svg viewBox="0 0 1200 350" className="curved-text-svg" preserveAspectRatio="xMidYMid meet">
-            <defs>
-              <path id="curve" d="M 100 300 Q 600 80 1100 300" />
-              <linearGradient id="rainbow-gradient" x1="0" x2="1" y1="0" y2="0">
-                <stop offset="0%" stopColor="#ff0000" />
-                <stop offset="14%" stopColor="#ff7f00" />
-                <stop offset="28%" stopColor="#ffff00" />
-                <stop offset="42%" stopColor="#00ff00" />
-                <stop offset="56%" stopColor="#0000ff" />
-                <stop offset="70%" stopColor="#4b0082" />
-                <stop offset="84%" stopColor="#8f00ff" />
-              </linearGradient>
-              <filter id="shadow">
-                <feDropShadow dx="3" dy="3" stdDeviation="3" floodColor="black" floodOpacity="1"/>
-              </filter>
-            </defs>
-            <text width="1200">
-              <textPath href="#curve" startOffset="50%" textAnchor="middle" fill="url(#rainbow-gradient)" filter="url(#shadow)">
-                Masal Fabrikasına Hoşgeldiniz
-              </textPath>
-            </text>
-          </svg>
+          <h1 className="bouncy-title">
+            <span style={{"--i": 0}}>M</span>
+            <span style={{"--i": 1}}>a</span>
+            <span style={{"--i": 2}}>s</span>
+            <span style={{"--i": 3}}>a</span>
+            <span style={{"--i": 4}}>l</span>
+            <span style={{"--i": 5}}> </span>
+            <span style={{"--i": 6}}>F</span>
+            <span style={{"--i": 7}}>a</span>
+            <span style={{"--i": 8}}>b</span>
+            <span style={{"--i": 9}}>r</span>
+            <span style={{"--i": 10}}>i</span>
+            <span style={{"--i": 11}}>k</span>
+            <span style={{"--i": 12}}>a</span>
+            <span style={{"--i": 13}}>s</span>
+            <span style={{"--i": 14}}>ı</span>
+            <span style={{"--i": 15}}>n</span>
+            <span style={{"--i": 16}}>a</span>
+            <span style={{"--i": 17}}> </span>
+            <span style={{"--i": 18}}>H</span>
+            <span style={{"--i": 19}}>o</span>
+            <span style={{"--i": 20}}>ş</span>
+            <span style={{"--i": 21}}>g</span>
+            <span style={{"--i": 22}}>e</span>
+            <span style={{"--i": 23}}>l</span>
+            <span style={{"--i": 24}}>d</span>
+            <span style={{"--i": 25}}>i</span>
+            <span style={{"--i": 26}}>n</span>
+            <span style={{"--i": 27}}>i</span>
+            <span style={{"--i": 28}}>z</span>
+          </h1>
         </div>
         <p>Resimlerinizi yükleyin, bir tema seçin ve yapay zeka sizin için özel bir masal oluştursun!</p>
         <button onClick={onNext} className="magic-button">
