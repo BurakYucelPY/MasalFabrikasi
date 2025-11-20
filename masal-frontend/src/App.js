79205027d20e2db74ad7badaf7b992677b434092
karub360 <karub360@gmail.com>
2025-11-20 23:19:51 +0300

    Mouse efekti (SplashCursor) eklendi

--- masal-frontend/src/App.js
+++ masal-frontend/src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Hosgeldin from './pages/Hosgeldin';
 import TemaSecimi from './pages/TemaSecimi';
 import MasalOlustur from './pages/MasalOlustur';
+import SplashCursor from './components/SplashCursor';
 import './App.css';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
 
   return (
     <div className="App">
+      <SplashCursor />
       {sayfa === 1 && <Hosgeldin onNext={() => setSayfa(2)} />}
       {sayfa === 2 && <TemaSecimi onSelectTheme={handleTemaSecimi} />}
       {sayfa === 3 && <MasalOlustur tema={secilenTema} />}
