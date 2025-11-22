b2ebd39a528c44a3dcff9581d69659cc374799a2
karub360 <karub360@gmail.com>
2025-11-22 20:17:28 +0300

    CORS ayarlarinda GitHub Pages adresi eklendi

--- api.py
+++ api.py
@@ -10,7 +10,10 @@ app = FastAPI()
 # CORS - React için
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://burakyucelpy.github.io"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
